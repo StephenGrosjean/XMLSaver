a2f57c144252f544b56efaade0398bd848791611
Stephen Grosjean <stephen-gm@hotmail.com>
2019-01-23 00:57:37 +0100

    First Version

---
+++ Assets/XMLSaver/Exemple/Visualizer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Visualizer for the XMLSave (Only for the test Dataset)
+/// </summary>
+public class Visualizer : MonoBehaviour {
+
+    [SerializeField] private GameObject firstDataPrefab; //First data visualizer
+    [SerializeField] private GameObject secondDataPrefab; //Second data visualizer
+    [SerializeField] private XMLSave xmlSaveScript; //The XMLSave script
+
+    [SerializeField] private Transform dataContainer; //Where the data blocs will be displayed
+
+    private List<GameObject> dataVisu = new List<GameObject>(); //List of all blocs
+
+    //Load the XML (Button usage)
+    public void LoadXML() {
+
+        //Destroy all blocs if there is any
+        if (dataVisu.Count != 0) {
+            foreach (GameObject datavisu in dataVisu) {
+                Destroy(datavisu);
+            }
+        }
+        //Clear the list
+        dataVisu.Clear();
+
+        //Load the XML
+        xmlSaveScript.Load();
+
+        //Display the blocs
+        Display();
+    }
+
+
+    //Instantiate blocs 
+    void Display() {
+
+        //Find all dataset in the firstDB 
+        foreach(FirstDataset dataset in xmlSaveScript.dataBase.firstDB) {
+            //Instantiate a bloc
+            GameObject visu = Instantiate(firstDataPrefab, transform.position, transform.rotation); 
+
+            //Set the parent of the bloc
+            visu.transform.parent = dataContainer;
+
+            //Set the color to green
+            visu.GetComponent<Image>().color = Color.green; 
+
+            //Set the name to dataset.name
+            visu.transform.Find("Name").GetComponent<Text>().text = dataset.name;
+
+            //Set the value to dataset.value (convert to string)
+            visu.transform.Find("Value").GetComponent<Text>().text = dataset.value.ToString();
+
+            //Add the bloc to the list
+            dataVisu.Add(visu);
+
+        }
+
+        //Same thing ad the first dataset with the second dataset
+        foreach (SecondDataset dataset in xmlSaveScript.dataBase.secondDB) {
+            GameObject visu = Instantiate(secondDataPrefab, transform.position, transform.rotation);
+            visu.transform.parent = dataContainer;
+            visu.GetComponent<Image>().color = Color.red;
+
+            visu.transform.Find("Name").GetComponent<Text>().text = dataset.name;
+            visu.transform.Find("Vector").GetComponent<Text>().text = dataset.pos.ToString();
+
+            dataVisu.Add(visu);
+        }
+    }
+}
---
+++ Assets/XMLSaver/XMLSave.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+/// <summary>
+/// Made by Stephen Grosjean
+/// V 0.1
+/// </summary>
+
+public class XMLSave : MonoBehaviour {
+
+    //SINGLETON//
+    public static XMLSave instance;
+
+    private void Awake() {
+        instance = this;
+    }
+    //END SINGLETON//
+
+    //Variables
+    [SerializeField] private string nameFile = "database.xml";
+    [SerializeField] private string path = "/XMLSaver/StreamingAssets/";
+
+    //Database item
+    public DataBase dataBase;
+
+
+    //Save function
+    public void Save() {
+        //open xml file
+        XmlSerializer serializer = new XmlSerializer(typeof(DataBase));
+
+        //Create Stream
+        FileStream stream = new FileStream(Application.dataPath + path + nameFile, FileMode.Create);
+        //Serialize DB
+        serializer.Serialize(stream, dataBase);
+
+        //Close the stream
+        stream.Close();
+    }
+
+    //Load function
+    public void Load() {
+        //open xml file
+        XmlSerializer serializer = new XmlSerializer(typeof(DataBase));
+        
+        //Create Stream
+        if (File.Exists(Application.dataPath + path + nameFile)) {
+            FileStream stream = new FileStream(Application.dataPath + path + nameFile, FileMode.Open);
+
+            //Deserialize DB
+            dataBase = serializer.Deserialize(stream) as DataBase;
+
+            //Close the stream
+            stream.Close();
+        }
+        else {
+            //Error message
+            Debug.LogErrorFormat("<b><size=12><color=red>ERROR : File " + nameFile + " does not exist at path : </color><color=green> " + path + " </color> <color=red> (Maybe you did not save?) </color></size></b>");
+        }
+    }
+
+
+}
+
+//What to save 
+[System.Serializable]
+public class DataBase {
+
+    [XmlArray("First_Dataset")] //Name in XML file (avoid spaces)
+    public List<FirstDataset> firstDB = new List<FirstDataset>(); //Class to save
+
+    [XmlArray("Second_Dataset")] 
+    public List<SecondDataset> secondDB = new List<SecondDataset>(); 
+}
+
+
+//Data type info
+[System.Serializable]
+public class FirstDataset {
+    public string name;
+    public int value;
+}
+
+//Data type info
+[System.Serializable]
+public class SecondDataset {
+    public string name;
+    public Vector2 pos;
+}
+
+
